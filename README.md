cf18988f1c06280ea004661f3fcb1db19a6881ff
TaiseiIto <taisei1212@outlook.jp>
2026-08-02 21:24:07 +0900

    Specify repository and branch #1

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # osdev-jp-tester
 Test [osdev.jp](https://github.com/osdev-jp/osdev-jp.github.io) locally
+
+```
+/somewhere $ git clone https://github.com/TaiseiIto/osdev-jp-tester.git
+/somewhere $ cd osdev-jp-tester
+/somewhere/osdev-jp-tester $ ./run.sh -r https://github.com/osdev-jp/osdev-jp.github.io -b master
+```
--- run.sh
+++ run.sh
@@ -13,10 +13,22 @@ image_exists() {
 	[ -f $image ] && docker image inspect $(< $1)
 }
 
+parse_arguments() {
+	while getopts "r:b:" argument; do
+		case $argument in
+			r) repository=$OPTARG;;
+			b) branch=$OPTARG;;
+		esac
+	done
+}
+
 remove_image() {
 	docker image rm $(< $1)
 	rm $1
 }
 
+parse_arguments $@
+echo $repository
+echo $branch
 image_exists $image && remove_image $image
 image_exists $image || build_image $image $docker_file
